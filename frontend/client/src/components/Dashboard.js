e46fac1b93f226ebb02f2c0a83a36d01d08a7fe2
Manav <patelmanav278@gmail.com>
2024-02-13 00:58:51 +0530

    [Updated/Added]: Parameters of Dashboard Component Added

--- frontend/client/src/components/Dashboard.js
+++ frontend/client/src/components/Dashboard.js
@@ -1,9 +1,49 @@
 import React from 'react'
-
+import '../App.css'
 export default function Dashboard() {
     return (
         <div>
-            <button type="button" class="btn btn-primary">Primary</button>
+            <div className="container-fluid dash text-white" >
+                <div className="row dashinner1 text-center">
+                    <div className="col-lg-12 bg-secondary">
+                        <h1>Title/Tagline of ProRefer</h1>
+                    </div>
+                </div>
+                <div className="row dashinner2 text-center">
+                    <div className="col-lg-5 col-md-6 col-sm-12 bg-secondary" style={{ border: "2px solid black" }}>
+                        <div>
+                            <button type="button" class="btn btn-dark">Edit Profile</button>
+                        </div>
+                        <div>
+                            <p>Name : Manav Jayesh Patel</p>
+                        </div>
+                        <div>
+                            <p>Location : Vadodara,Gujarat</p>
+                        </div>
+                        <div>
+                            <p>Referrals Requested : 100</p>
+                        </div>
+                        <div>
+                            <p>Referrals Received : 50</p>
+                        </div>
+                        <div>
+                            <p>Referrals Requests Received : 4</p>
+                        </div>
+                        <div>
+                            <p>Monthly Requests : 44</p>
+                        </div>
+                        <div>
+                            <p>Last_Referral_Date : 3/10/2003</p>
+                        </div>
+                        <div>
+                            <p>Warning : 3</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-7 col-md-6 col-sm-12 bg-secondary" style={{ border: "2px solid black" }}>
+                        Dashboard Receive Sent Companies
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
