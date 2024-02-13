85a4d05e22801ef42183b49c68d5a62000b49470
Manav <patelmanav278@gmail.com>
2024-02-13 12:56:28 +0530

    [Updated]: Dashboard Component Updated with appropriate properties

--- frontend/client/src/components/Dashboard.js
+++ frontend/client/src/components/Dashboard.js
@@ -3,55 +3,77 @@ import '../App.css'
 export default function Dashboard() {
     return (
         <div>
-            <div className="container-fluid dash text-white" >
+            {/* ----------------------------------------- */}
+            <div className="container-fluid dash">
 
-                {/* NavBar */}
+                {/* ----------------------------------------- */}
 
-                {/* Row1 */}
-
-                <div className="row dashinner1 text-center">
-                    <div className="col-lg-12 bg-secondary">
-                        <h1>Title/Tagline of ProRefer</h1>
+                <div class="container px-4 text-center">
+                    <div class="row gx-2">
+                        <div class="col-12">
+                            <div class="p-3 text-white">
+                                <h1>Title/Tagline of ProRefer</h1>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                {/* Row2 */}
+                {/* ----------------------------------------- */}
 
-                <div className="row dashinner2 text-center">
-                    <div className="col-lg-5 col-md-6 col-sm-12 bg-secondary" style={{ border: "2px solid black" }}>
-                        <div>
-                            <button type="button" class="btn btn-dark">Edit Profile</button>
+                <div class="container-fluid px-4 text-center">
+                    <div class="row gx-2">
+
+                        {/* *********************************** */}
+
+                        <div class="col-lg-4 col-md-5 col-sm-6">
+                            <div class="p-3 bg-secondary dashinner2 text-white">
+                                <div>
+                                    <button type="button" class="btn btn-dark">Edit Profile</button>
+                                </div>
+                                <div>
+                                    <p>Name : Manav Jayesh Patel</p>
+                                </div>
+                                <div>
+                                    <p>Location : Vadodara,Gujarat</p>
+                                </div>
+                                <div>
+                                    <p>Total Referrals Requested : 100</p>
+                                </div>
+                                <div>
+                                    <p>Total Referrals Received : 50</p>
+                                </div>
+                                <div>
+                                    <p>Monthly Requests Received : 4</p>
+                                </div>
+                                <div>
+                                    <p>Monthly Requests Sent : 44</p>
+                                </div>
+                                <div>
+                                    <p>Last Referral Date : 3/10/2003</p>
+                                </div>
+                                <div>
+                                    <p>Warning : 3</p>
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            <p>Name : Manav Jayesh Patel</p>
+
+                        {/* *********************************** */}
+
+                        <div class="col-lg-8 col-md-7 col-sm-6">
+                            <div class="p-3 bg-secondary dashinner2">
+                                <h1>Dashboard Receive Sent Companies</h1>
+                            </div>
                         </div>
-                        <div>
-                            <p>Location : Vadodara,Gujarat</p>
-                        </div>
-                        <div>
-                            <p>Referrals Requested : 100</p>
-                        </div>
-                        <div>
-                            <p>Referrals Received : 50</p>
-                        </div>
-                        <div>
-                            <p>Referrals Requests Received : 4</p>
-                        </div>
-                        <div>
-                            <p>Monthly Requests : 44</p>
-                        </div>
-                        <div>
-                            <p>Last_Referral_Date : 3/10/2003</p>
-                        </div>
-                        <div>
-                            <p>Warning : 3</p>
-                        </div>
-                    </div>
-                    <div className="col-lg-7 col-md-6 col-sm-12 bg-secondary" style={{ border: "2px solid black" }}>
-                        Dashboard Receive Sent Companies
+
+                        {/* *********************************** */}
+
                     </div>
                 </div>
+                {/* ----------------------------------------- */}
+
+
             </div>
+            {/* ----------------------------------------- */}
         </div>
     )
 }
