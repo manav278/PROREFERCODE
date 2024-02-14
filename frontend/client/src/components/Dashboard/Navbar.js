f722b251fed5d145fa59e949ebb487dde7f2cb12
aagammodi <modiaagam10@gmail.com>
2024-02-14 08:06:22 +0530

    [added]: Dashboard components added

--- frontend/client/src/components/Dashboard/Navbar.js
+++ frontend/client/src/components/Dashboard/Navbar.js
@@ -1,13 +1,36 @@
 import React, {useState} from 'react'
-
+import Db from "./Db";
+import Received from "./Received";
+import Sent from "./Sent";
+import Company from "./Company";
 const CustomNavbar = () => {
 
+  const [mode, setMode] = useState('Dashboard');
+
+  const handleDashboardClick = () => {
+    setMode('Dashboard');
+  }
+  const handleReceivedClick = () => {
+    setMode('Received');
+  }
+  const handleSentClick = () => {
+    setMode('Sent');
+  }
+  const handleCompanyClick = () => {
+    setMode('Company');
+  }
+
     return (
-    <div>
-      {/* <button>Dashboard</button>
-      <button>Received</button>
-      <button>Sent</button>
-      <button>Companies</button> */}
+    <div className='navbuttons'>
+      <button onClick={handleDashboardClick}>Dashboard</button>
+      <button onClick={handleReceivedClick}>Received</button>
+      <button onClick={handleSentClick}>Sent</button>
+      <button onClick={handleCompanyClick}>Companies</button>
+      <hr style={{color:'white'}}/>
+      {(mode==='Dashboard' && <Db/>)}
+      {(mode==='Received' && <Received/>)}
+      {(mode==='Sent' && <Sent/>)}
+      {(mode==='Company' && <Company/>)}
     </div>
     );
 }
