a25e3d32a8d4b454df1de675ff59f8da7fb4d84f
Manav <patelmanav278@gmail.com>
2024-02-14 11:29:56 +0530

    [Updated]: Home.js and db.js updated

--- frontend/client/src/components/Dashboard/Db.js
+++ frontend/client/src/components/Dashboard/Db.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const Db = () => {
   return (
-    <div>Db</div>
-  )
-}
+    <div class="container px-4 text-center" style={{backgroundColor:"black"}}>
+      <div class="row gx-5 align-items-center">
+        <div class="col">
+          {/* <table style={{color:'white'}}>
+            <tr>
+              <td style={{padding:'25px'}}>Seeking a referral for your dream position at your preferred company? You're in luck! Simply request it here. If you need more information, check out our FAQ section. We've got all the details you need to navigate the referral process smoothly. Don't miss out on this opportunity to boost your chances of landing your ideal role. Take the first step towards your career goals today!</td>
+              <td style={{padding:'25px'}}>
+              Ensure your resume is always current by updating it here. This updated version will serve as the basis for any referral requests you make. Keeping your resume up-to-date is crucial for seizing new opportunities and showcasing your skills effectively
+              </td>
+            </tr>
+            <tr>
+              <td style={{padding:'25px'}}>
+              <button type="button" class="btn btn-primary">
+                Request Referral
+              </button>
+              </td>
+              <td style={{padding:'25px'}}>
+              <button type="button" class="btn btn-primary">
+                Resume
+              </button>
+              </td>
+            </tr>
+          </table> */}
+          <div class="p-3">
+            <p style={{color:'white'}}>"Seeking a referral for your dream position at your preferred company? You're in luck! Simply request it here. If you need more information, check out our FAQ section. We've got all the details you need to navigate the referral process smoothly. Don't miss out on this opportunity to boost your chances of landing your ideal role. Take the first step towards your career goals today!"</p>
+          
+            <button type="button" class="btn btn-primary">
+              Request Referral
+            </button>
+          </div>
+        </div>
+        <div class="col">
+        <div class="p-3">
+          <p style={{color:'white'}}>Ensure your resume is always current by updating it here. This updated version will serve as the basis for any referral requests you make. Keeping your resume up-to-date is crucial for seizing new opportunities and showcasing your skills effectively</p>
+          
+            <button type="button" class="btn btn-primary">
+              Resume
+            </button>
+          </div>
+        </div>
+        {/* <hr style={{color:'white'}}/> */}
+      </div>
+    </div>
+  );
+};
 
-export default Db
+export default Db;
--- frontend/client/src/components/Dashboard/Home.js
+++ frontend/client/src/components/Dashboard/Home.js
@@ -22,16 +22,14 @@ export default function Home() {
                 {/* ----------------------------------------- */}
 
                 <div class="container-fluid px-4 text-center">
-                    <div class="row gx-2">
+                    <div class="row gx-2 ">
 
                         {/* *********************************** */}
 
-                        <div class="col-lg-4 col-md-5 col-sm-6">
+                        <div class="col-lg-4 col-md-5 col-sm-6 ">
                             <div class="p-3 bg-secondary dashinner2 text-white">
+                                <button style={{width:'80%'}} type="button" class="btn btn-primary">Edit Profile</button>
                                 <table className='table1'>
-                                <tr colSpan={2}>
-                                    <td colSpan={2}><button type="button" class="btn btn-primary">Edit Profile</button></td>
-                                </tr>
                                 <tr><td><br/></td></tr>
                                 <tr>
                                     <td><p>Name</p></td> 
