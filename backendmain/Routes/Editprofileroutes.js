1ecd43ffb8e4f2950c00551b8a0ae50178273fbe
aagammodi <modiaagam10@gmail.com>
2024-03-09 21:16:32 +0530

    [Updated]: OTP Verification for Applicant completed along with integration and error handling. AuthData updation taken care of.

--- backendmain/Routes/Editprofileroutes.js
+++ backendmain/Routes/Editprofileroutes.js
@@ -1,7 +1,59 @@
 import express from "express";
+import nodemailer from "nodemailer";
+import CryptoJS from "crypto-js";
 import { getUserId } from "./Loginroutes.js";
 import proreferuser from "../Model/proreferuser.js";
+import authdata from "../Model/authdata.js";
+import * as env from "dotenv";
+import { readSync } from "fs";
+env.config();
 const router = express.Router();
+let globalEncryptedOTP, globalSalt;
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+const generateOTP = () => {
+  return Math.floor(100000 + Math.random() * 900000);
+};
+
+const sendOTPByEmail = async (email) => {
+  const otp = generateOTP();
+
+  const salt = CryptoJS.lib.WordArray.random(128 / 8);
+  const encryptedOTP = CryptoJS.AES.encrypt(
+    otp.toString(),
+    salt.toString()
+  ).toString();
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: email,
+    subject: "OTP for verification",
+    text: `Your OTP is ${otp}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    return { encryptedOTP, salt };
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return null;
+  }
+};
+
+const verifyOTP = (encryptedOTP, enteredOTP) => {
+  const decryptedOTP = CryptoJS.AES.decrypt(
+    encryptedOTP,
+    globalSalt.toString()
+  ).toString(CryptoJS.enc.Utf8);
+  return enteredOTP === decryptedOTP;
+};
 
 router.post("/getEditProfileDetails", async (req, res) => {
   try {
@@ -16,9 +68,16 @@ router.post("/getEditProfileDetails", async (req, res) => {
 
 router.post("/updateApplicant", async (req, res) => {
   try {
-    const { mobileNumber, personalEmail, location } = req.body;
-    // console.log(mobileNumber, ' ', personalEmail, ' ', location);
+    const { mobileNumber, personalEmail, location, isPersonalEmailChanged } =
+      req.body;
     let ID = getUserId();
+    console.log(isPersonalEmailChanged);
+    let oldEmail;
+    if (isPersonalEmailChanged) {
+      let oldUser = await proreferuser.findOne({ User_ID: ID });
+      oldEmail = oldUser.Personal_Email;
+      console.log('oldEmail: ', oldEmail);
+    }
     await proreferuser.findOneAndUpdate(
       { User_ID: ID },
       {
@@ -27,6 +86,11 @@ router.post("/updateApplicant", async (req, res) => {
         COMPANY_LOCATION: location,
       }
     );
+    if (isPersonalEmailChanged)
+      await authdata.findOneAndUpdate(
+        { Personal_Email: oldEmail },
+        { Personal_Email: personalEmail }
+      );
     res.json({ message: "Update successful" });
   } catch (error) {
     console.log("Error fetching data from MongoDB: ", error);
@@ -47,6 +111,32 @@ router.post("/updateEmployee", async (req, res) => {
   } catch (error) {
     console.log("Error fetching data from MongoDB: ", error);
     res.json({ message: "Internal server error from backend" });
+  }
+});
+
+router.post("/requestOtp", async (req, res) => {
+  try {
+    const { personalEmail } = req.body;
+    let { encryptedOTP, salt } = await sendOTPByEmail(personalEmail);
+    globalEncryptedOTP = encryptedOTP;
+    globalSalt = salt;
+    console.log(encryptedOTP);
+    res.json({ message: "Otp sent" });
+  } catch (error) {
+    console.log("Error: ", error);
+    res.json({ message: "Error trying to send OTP" });
+  }
+});
+
+router.post("/verifyOtp", async (req, res) => {
+  try {
+    const { otp } = req.body;
+    const isCorrect = verifyOTP(globalEncryptedOTP, otp);
+    if (isCorrect) res.json({ message: "Otp verified" });
+    else res.json({ message: "Otp incorrect" });
+  } catch (error) {
+    console.log("Error: ", error);
+    res.json({ message: "Error trying to verify OTP" });
   }
 });
 
